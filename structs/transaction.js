d3fd1ee54e6c38418407c1aa4f16054d2090d8cc
ArnaudovSt <telerikacademy2016@gmail.com>
2018-03-06 21:44:44 +0200

    added transaction template

---
+++ structs/transaction.js
@@ -0,0 +1,16 @@
+class Transaction {
+    constructor(from, to, value, fee, dateCreated, senderPubKey, senderSignature, transactionHash, minedInBlockIndex, transferSuccessful) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+        this.fee = fee;
+        this.dateCreated = dateCreated
+        this.senderPubKey = senderPubKey;
+        this.senderSignature = senderSignature;
+        this.transactionHash = transactionHash;
+        this.minedInBlockIndex = minedInBlockIndex;
+        this.transferSuccessful = transferSuccessful
+    }
+}
+
+module.exports = Transaction
